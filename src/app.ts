9f3b75d522033cd713ba2664cd2999128a03efe3
Gonzalo Monja <gonzalormonja1@gmail.com>
2024-04-16 10:44:51 -0300

    fix: change console.error to console.info

--- src/app.ts
+++ src/app.ts
@@ -9,18 +9,18 @@ async function main() {
 	const importDebtorService = new ImportDebtorsService();
 	await importDebtorService.importDebtors();
 
-	console.error('End import debtors');
+	console.info('End import debtors');
 }
 
 mongoose
 	.connect(env.MONGODB_URI)
 	.then(async () => {
-		console.error('Database connected successfully');
+		console.info('Database connected successfully');
 
 		//this is only for testing purposes
 		await resetDatabase();
 
-		console.error('End truncate database');
+		console.info('End truncate database');
 
 		await main();
 	})
